595dbdd61fdfba31e4403f130e5c3ff527f27ec3
happyHooman <tornea.alexandru@gmail.com>
2018-05-29 23:00:03 +0300

    added start event handler
    trying to remove the event

--- app/js/game.js
+++ app/js/game.js
@@ -1,47 +1,62 @@
 class GameRunner {
-  constructor() {
-    this.currentSpeed = 4;
-    this.points = 0;
-    this.acceleration = 0.001;
-  }
+	constructor() {
+		let me = this;
+		me.currentSpeed = 4;
+		me.points = 0;
+		me.acceleration = 0.001;
+		me.background = new Background();
+		me.biker = new Biker();
+		me.init()
+	}
 
-  start() {
-  	if (!this.isRunning){
-        let me = this;
-  		me.isRunning = true;
-        me.background = new Background();
-        me.biker = new Biker();
-        me.obstacle = new Obstacle();
-        me.moveObstacles();
-        me.init();
-	  }
-  }
+	start() {
+		console.log('game started');
+		let me = this;
+		if (!me.isRunning) {
+			me.isRunning = true;
+			me.startEventHandler();
+			me.obstacle = new Obstacle();
+			me.moveObstacles();
+		}
+	}
 
-  init(){
-      let me = this;
-      window.addEventListener('keydown', function (e) {
-          if (e.keyCode === 32){
-              me.gameOver()
-          }
-      })
-  }
+	init() {
+		this.startEventHandler();
+	}
 
-  moveObstacles(){
-      let me = this;
-      me.points++;
-      me.currentSpeed+= me.acceleration;
-      me.obstacle.render();
-      me.obstacle.cPos.x = me.obstacle.cPos.x > -me.obstacle.width ? me.obstacle.cPos.x - me.currentSpeed : 1000;
-      me.animID = window.requestAnimationFrame(function () {
-          me.moveObstacles();
-      });
-  }
+	startEventHandler() {
+		let me = this;
+		let handlerFunc = function (e) {
+			if (e.keyCode === 32 || e.keyCode === 38) {
+				me.start()
+			}
+		};
+		let handler = handlerFunc.bind(me);
 
-  gameOver() {
-    window.cancelAnimationFrame(this.animID);
-  }
+		if (!me.isRunning) {
+			window.addEventListener('keydown', handler);
+		} else {
+			window.removeEventListener('keydown', handler);
+			console.log('removed event handler')
+		}
+	}
+
+	moveObstacles() {
+		let me = this;
+		me.points++;
+		me.currentSpeed += me.acceleration;
+		me.obstacle.render();
+		me.obstacle.cPos.x = me.obstacle.cPos.x > -me.obstacle.width ? me.obstacle.cPos.x - me.currentSpeed : 1000;
+		me.animID = window.requestAnimationFrame(function () {
+			me.moveObstacles();
+		});
+	}
+
+	gameOver() {
+		window.cancelAnimationFrame(this.animID);
+	}
 }
 
 let game = new GameRunner();
 
-game.start();
+// game.start();
