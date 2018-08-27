bc9621ab84fd5746ea5e549adda7f2ba35b73ed8
Alex <tornea.alexandru@gmail.com>
2018-08-27 14:05:33 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # ML-Game
-[Demo preview](http://bb.assist.ro/biker-game/)
+[Preview](http://bb.assist.ro/biker-game/)
 
 
+We wanted to practice our AI skills so we have decided to make a simple game with 2 inputs. 
+The idea of the game is inspired from the Google Chrome browser dino game when in offline mode. 
+The technology used is HTML5 <canvas\> element and plain ES6 Javascript. 
+During game development we also have tried to use best practices and make a clean, easy to understand, and maintainable
+code.
 
-The main purpose of this project is to study AI &amp; ML techniques and implementations. 
-The ML algorithms will learn to play a Game.
-
+Please feel free to advise any improvements. Can't wait to get as smart as you are!
--- src/js/ScoreDisplay.js
+++ src/js/ScoreDisplay.js
@@ -1,61 +1,20 @@
-import Digit from "./Digit";
-import Sprite from "./Sprite";
-import img from "../img/environment_sheet.png";
-import {assetsOptions} from "./AssetsOptions";
+import img from '../img/environment_sheet.png'
+import Sprite from './Sprite'
 
-export default class ScoreDisplay {
-	constructor() {
-		this.canvas = document.getElementById("biker-layer");
-	}
+export default class Rock{
+	constructor(pos, type) {
+		let originX = type === 2 ? 100 : 0;
 
-	showScore(score) {
-		this.displayNumber(score,0);
-	}
-
-	showGameOver() {
-		let pos = {
-			x: (1000 - assetsOptions.game_over.width) / 2,
-			y: 110
-		};
-		this.createAsset(assetsOptions.game_over, pos);
-	}
-
-	showHIScore(score) {
-		let me = this,
-				pos = {
-					x: 1000 - 300,
-					y: 0
-				};
-		me.createAsset(assetsOptions.HI, pos);
-		me.displayNumber(score,6);
-	}
-
-	createAsset(options, pos) {
-		let a = new Sprite({
-			canvas: this.canvas,
+		super({
+			canvas: document.getElementById("obstacle-layer"),
 			imageSrc: img,
-			width: options.width,
-			height: 26,
-			origin: {x: options.x, y: 0},
-			pos
-		});
-		a.draw();
-	}
-
-	displayNumber(number, pos = 0) {
-		let me = this,
-				scStr = number.toString(),
-				len = scStr.length;
-
-		me.canvas.getContext('2d').clearRect(me.canvas.width - 115, 0, 115, 30);
-
-		for (let i = 0; i < 5; i++) {
-			if (i < len) {
-				new Digit(scStr[len - i - 1], i + pos);
-			} else {
-				new Digit(0, i + pos);
+			width: 100,
+			height: 90,
+			origin: {x: originX, y: 128},
+			pos: {
+				x: pos,
+				y: 175
 			}
-		}
+		});
 	}
-
 }
